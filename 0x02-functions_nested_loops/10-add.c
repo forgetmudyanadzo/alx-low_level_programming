7e21a87d9a04edfc9f1937635a49910f4ce8697c
forgetmudyanadzo <forgetmudyanadzo87@gmail.com>
2023-02-21 14:59:02 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * Add - add two integers
+ * @x: int to be added to y
+ * @y: int to be added to x
+ * Return: sum of x and y
+*/
+
+int add(int x, int y)
+{
+	int c;
+
+	c = x + y;
+
+	return (c);
+}
