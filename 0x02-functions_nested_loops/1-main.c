5243a00e49ed4c59783d56f0218b9d3268b8551a
forgetmudyanadzo <forgetmudyanadzo87@gmail.com>
2023-02-21 07:12:16 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * main - write the character c to store
+ * c: the character to print
+ *
+ * Return: On success 1
+*/
+int main(void)
+{
+	char letter;
+		
+	for (letter = 'a'; letter <= 'z'; letter++);
+
+	return (0);
+}
