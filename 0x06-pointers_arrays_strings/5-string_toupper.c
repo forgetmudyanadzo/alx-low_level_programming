2d4410159698e24fe68cfa1f70af55b73975fd72
forgetmudyanadzo <forgetmudyanadzo87@gmail.com>
2023-03-01 12:38:48 -0800

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase of a string to uppercase
+ * @n: pointer
+ *
+ * Return: n
+*/
+
+char *string_toupper(char *n)
+
+{
+	int c;
+
+	c = 0;
+	while (n[c] != '\0')
+	{
+		if (n[c] >= 'a' && n[c] <= 'z')
+			n[c] = n[c] - 32;
+		c++;
+	}
+	return (n);
+}
