df5d21a5287c4cbb01a176ce8d36f9cbb4c2a4ca
forgetmudyanadzo <forgetmudyanadzo87@gmail.com>
2023-06-20 23:36:21 -0700

    Task 0 Solution

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _strlen - Returns the length of a string
+ * @str: The sting to get the lengthof
+ * Return: the length of @str
+*/
+size_t _strlen(const char *str)
+
+{
+	size_t length = 0;
+
+	while (*str++)
+	length++;
+	return (length);
+}
